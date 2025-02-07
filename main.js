c061af79eeebe42bcea3bff343c46de54e8ffffd
Nerwen <jenny2209@gmx.de>
2025-02-07 16:32:37 +0100

    improve removeCurrentGrid function

--- main.js
+++ main.js
@@ -23,13 +23,10 @@ function cleanCanvas() {
 }
 
 function removeCurrentGrid() {
-  let pixelList = document.querySelectorAll(".pixel");
-
-  pixelList.forEach((pixel) => canvas.removeChild(pixel));
+  canvas.innerHTML = "";
 }
 
 function drawGrid(gridSize = 16) {
-  //const border = 2;
   let pixelAmount = gridSize * gridSize;
   let canvasWidth = canvas.clientWidth;
   let pixelSize = canvasWidth / gridSize;
